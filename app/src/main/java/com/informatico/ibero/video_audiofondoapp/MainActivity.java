953f1247a340c3051e003b557f1e3509c9303d87
El Informatico Ibero <tamani1995@gmail.com>
2022-12-07 10:45:04 -0500

    Actualizaciòn del Proyecto del Video

--- app/src/main/java/com/informatico/ibero/video_audiofondoapp/MainActivity.java
+++ app/src/main/java/com/informatico/ibero/video_audiofondoapp/MainActivity.java
@@ -14,12 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*Para el video*/
+        /*Para el video*/
         VideoView videoup=(VideoView)findViewById(R.id.Videovup);
         //activamos el controlador
         MediaController mc = new MediaController(this);
+        mc.setAnchorView(videoup);
         //direccion del video
-        Uri path = Uri.parse("android.resource://informatico.to.audioapp/"+R.raw.video1);
+        //Uri path = Uri.parse("android.resource://com.informatico.ibero.video_audiofondoapp/"+R.raw.cat1);
+        Uri path = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.cat1);
         //lo damos el controlador al video
         videoup.setMediaController(mc);
         //le damos la direccion al video de donde se cargara
@@ -27,14 +29,5 @@ public class MainActivity extends AppCompatActivity {
         //Lanzamos el video para su reproduccion
         videoup.start();
 
-    }
-    public void MusicadeFondo(){
- /*musica de fondo*/
-//Creamos un objeto de la clase para darla la ruta del archivo
-        MediaPlayer reproductor=MediaPlayer.create(this,R.raw.musica1);
-        //setLooping nospermite que se vuelva a reproducir el audio
-        reproductor.setLooping(true);
-        //Iniciamos la reproduccion de audio audio
-        reproductor.start();
     }
 }
